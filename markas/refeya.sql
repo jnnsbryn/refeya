9b3a386bd95af134d97b271ea48854261679600f
☮ <48197495+jeannesbond@users.noreply.github.com>
2024-06-07 00:21:36 +0700

    Update refeya.sql

--- markas/refeya.sql
+++ markas/refeya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2024 at 07:18 PM
+-- Generation Time: Jun 06, 2024 at 07:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -33,8 +33,19 @@ CREATE TABLE `tbl_cafe` (
   `deskripsi_cafe` text NOT NULL,
   `lokasi_cafe` varchar(30) NOT NULL,
   `fasilitas_cafe` varchar(60) NOT NULL,
-  `foto_cafe` varchar(100) NOT NULL
+  `foto_cafe` varchar(100) NOT NULL,
+  `input_date` date NOT NULL,
+  `updated_date` date NOT NULL,
+  `status_cafe` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_cafe`
+--
+
+INSERT INTO `tbl_cafe` (`id_cafe`, `nama_cafe`, `deskripsi_cafe`, `lokasi_cafe`, `fasilitas_cafe`, `foto_cafe`, `input_date`, `updated_date`, `status_cafe`) VALUES
+(1, 'Cafe Maspion', 'Cafe pinggir pabrik buka 25 jam', 'https://google.com', 'Wifi,Listrik,Toilet', '06062024235111bicycle-1872682_1280.jpg', '2024-06-06', '2024-06-07', 1),
+(2, 'Cafe Aloha', 'Cafe pinggir alas buka 1 jam', 'https://bing.com', 'WiFi,Toilet', '06062024222008bazaar-1853361_1280.jpg', '2024-06-06', '2024-06-07', 0);
 
 -- --------------------------------------------------------
 
@@ -60,7 +71,8 @@ CREATE TABLE `tbl_review` (
   `id_cafe` int(11) NOT NULL,
   `rating_cafe` int(11) NOT NULL,
   `ulasan_cafe` text NOT NULL,
-  `tanggal_submit` date NOT NULL DEFAULT current_timestamp()
+  `tanggal_submit` date NOT NULL DEFAULT current_timestamp(),
+  `tanggal_update` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -71,19 +83,20 @@ CREATE TABLE `tbl_review` (
 
 CREATE TABLE `tbl_user` (
   `id_user` int(11) NOT NULL,
-  `name_user` varchar(16) NOT NULL,
-  `pwd_user` varchar(200) NOT NULL,
   `email_user` varchar(30) NOT NULL,
-  `role_user` varchar(5) NOT NULL
+  `pwd_user` varchar(200) NOT NULL,
+  `role_user` varchar(5) NOT NULL,
+  `status_user` int(11) NOT NULL DEFAULT 1,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`id_user`, `name_user`, `pwd_user`, `email_user`, `role_user`) VALUES
-(1, 'admin', '$2y$10$94D72fWs4MN6DWT1CPJw2uPp3fyTvdanuf84PozJ40h39GseIvBJ6', 'admin@refeya.my.id', 'admin'),
-(2, 'sugeng', '$2y$10$CxZTd9HFCk9AgISCvLstDOKQveCWI1dZB2Se/5kCG6sGInCf9pcO.', 'sugeng@gmail.com', 'user');
+INSERT INTO `tbl_user` (`id_user`, `email_user`, `pwd_user`, `role_user`, `status_user`, `created_at`) VALUES
+(1, 'admin@refeya.my.id', '$2y$10$94D72fWs4MN6DWT1CPJw2uPp3fyTvdanuf84PozJ40h39GseIvBJ6', 'admin', 1, '2024-06-06'),
+(2, 'sugeng@gmail.com', '$2y$10$CxZTd9HFCk9AgISCvLstDOKQveCWI1dZB2Se/5kCG6sGInCf9pcO.', 'user', 1, '2024-06-06');
 
 --
 -- Indexes for dumped tables
@@ -121,7 +134,7 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_cafe`
 --
 ALTER TABLE `tbl_cafe`
-  MODIFY `id_cafe` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cafe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_favorite`
